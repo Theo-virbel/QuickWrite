633a116466cc760a90c4d45e95e6d2142e246cc9
theovbl <tvirbel@gmail.com>
2024-08-19 16:30:14 +0200

    travaille sur script

---
+++ scripts/config.js
@@ -0,0 +1,10 @@
+/*********************************************************************************
+ * 
+ * Ce fichier contient toutes les constantes nécessaires au fonctionnement du jeu.
+ * En particulier les listes de mots et de phrases proposés à l'utilisateur
+ * 
+ *********************************************************************************/
+
+// Déclaration des tableaux contenant les listes des mots proposés à l'utilisateur
+const listeMots = ["Maman", "Blanc", "Inconstitutionnellement", "Rouge"]
+const listePhrases = ["Je suis là !", "La vie, l'univers et le reste", "Merci pour le poisson", "Je veux un cahier de couleur jaune"]
